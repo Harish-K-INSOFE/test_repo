44573c73d3bf1dbea48815738caea1c3d9270be5
Harish-Kumar-K <kharishk1998@gmail.com>
2021-04-22 20:34:52 +0530

    Conflict Resolved in line 9

--- test1.py
+++ test1.py
@@ -6,8 +6,4 @@ print("Hello world")
 
 #This line changed in local again changed in github
 
-<<<<<<< HEAD
 #Conflict line local 
-=======
-#Conflict line github
->>>>>>> ca6f2582e0c1fb605566467aad98207012cd3789
